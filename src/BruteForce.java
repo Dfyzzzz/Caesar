a9e718dcf277ef34811e25d010e12229012bc270
Dfyz <zim.vanya@yandex.by>
2023-09-05 16:55:04 +0300

    using OOP

--- src/BruteForce.java
+++ src/BruteForce.java
@@ -1,8 +1,3 @@
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 public class BruteForce {
     private static final double averageWordLength = 7.2;
     public static int bruteForce(){
